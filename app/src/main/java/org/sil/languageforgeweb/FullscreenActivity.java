12d0c02956896cd60093eccd306460ec0fd54a43
megahirt <chris@hirtfamily.net>
2017-06-01 10:52:23 +0700

    save/restore URL state on app stop/start

--- app/src/main/java/org/sil/languageforgeweb/FullscreenActivity.java
+++ app/src/main/java/org/sil/languageforgeweb/FullscreenActivity.java
@@ -1,7 +1,9 @@
 package org.sil.languageforgeweb;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebView;
@@ -22,11 +24,34 @@ public class FullscreenActivity extends AppCompatActivity {
         mContentView = findViewById(R.id.webview);
 
         WebView view = (WebView) mContentView;
-        view.setWebViewClient(new WebViewClient());
-        view.loadUrl("https://languageforge.org");
+
+        // Restore last URL
+        SharedPreferences settings = getPreferences(MODE_PRIVATE);
+        String currentUrl = settings.getString("currentUrl", null);
+
+        if (TextUtils.isEmpty(currentUrl)) {
+            view.setWebViewClient(new WebViewClient());
+            view.loadUrl("https://languageforge.org");
+        } else {
+            view.loadUrl(currentUrl);
+        }
+
         view.getSettings().setJavaScriptEnabled(true);
+        view.getSettings().setAppCacheEnabled(true);
 
+    }
 
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        SharedPreferences settings = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        WebView view = (WebView) mContentView;
+        editor.putString("currentUrl", view.getUrl());
+
+        // Commit the edits!
+        editor.commit();
     }
 
     @Override
