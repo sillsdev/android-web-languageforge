79fd00345ef5d06a12fa1b374aad7b17e07cdc9a
megahirt <chris@hirtfamily.net>
2017-06-01 14:48:08 +0700

    add indicator when browser is loading

--- app/src/main/java/org/sil/languageforgeweb/FullscreenActivity.java
+++ app/src/main/java/org/sil/languageforgeweb/FullscreenActivity.java
@@ -6,8 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -15,6 +17,7 @@ import android.webkit.WebViewClient;
  */
 public class FullscreenActivity extends AppCompatActivity {
     private View mContentView;
+    private View mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +25,30 @@ public class FullscreenActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_fullscreen);
         mContentView = findViewById(R.id.webview);
+        mProgressBar = findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(View.VISIBLE);
 
         WebView view = (WebView) mContentView;
+
+        // Setup Progress Indicator
+        ProgressBar mProgress = (ProgressBar) mProgressBar;
+        mProgress.setIndeterminate(true);
+        view.setWebChromeClient(new WebChromeClient(){
+            public void onProgressChanged(WebView view, int progress) {
+                mProgressBar.setVisibility(View.VISIBLE);
+                if(progress == 100) {
+                    mProgressBar.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+        view.setWebViewClient(new WebViewClient());
 
         // Restore last URL
         SharedPreferences settings = getPreferences(MODE_PRIVATE);
         String currentUrl = settings.getString("currentUrl", null);
 
+
         if (TextUtils.isEmpty(currentUrl)) {
-            view.setWebViewClient(new WebViewClient());
             view.loadUrl("https://languageforge.org");
         } else {
             view.loadUrl(currentUrl);
@@ -38,6 +56,7 @@ public class FullscreenActivity extends AppCompatActivity {
 
         view.getSettings().setJavaScriptEnabled(true);
         view.getSettings().setAppCacheEnabled(true);
+
 
     }
 
